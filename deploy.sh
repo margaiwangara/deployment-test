22c7f2591c4063b2affe5febeb14eb6203317dcd
margaiwangara <margaiwangara@gmail.com>
2020-02-26 10:30:08 +0300

    modified deploy sh file

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 set -e
 
 sudo apt-get install -y lftp
-mkdir deployment-test
+mkdir deployment
 
 # deployment via ftp upload, using ftps
 lftp -c "set ftps:initial-prot '';set ssl:verify-certificate no;
-        open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv deployment-test .; quit;"
+        open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv deployment .; quit;"
