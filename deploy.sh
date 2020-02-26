cc82b64478d2a3a6df9acf9650c35cc3955f06ba
margaiwangara <margaiwangara@gmail.com>
2020-02-26 09:34:03 +0300

    added deployment script for test #1

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#deploy sh
+set -e
+
+sudo apt-get install -y lftp
+
+# deployment via ftp upload, using ftps
+lftp -c "set ftps:initial-prot '';set ftp:ssl-force true:set ftp:ssl-protect-data true;
+        open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv deployment-test .; quit;"
